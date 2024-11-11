0870eb289c42a10e58ae0b946ddb5853ca506c0f
xXMaddyX <maddyaion@hotmail.de>
2024-11-11 10:52:00 +0100

    update UI

--- src/UXSystem/APP/AlertBoxes/AlertBoxes.js
+++ src/UXSystem/APP/AlertBoxes/AlertBoxes.js
@@ -4,6 +4,9 @@ class AlertBoxes extends HTMLElement {
     constructor() {
         super();
         this.shadow = this.attachShadow({mode: "open"});
+        this.followerButtonState = false;
+        this.rentButtonState = false;
+        this.subscribeButtonState = false;
     };
 
     async connectedCallback () {
@@ -20,11 +23,38 @@ class AlertBoxes extends HTMLElement {
     };
 
     selectors() {
-
+        this.followerActiveButton = this.shadow.querySelector('#follower-alert-activate-button');
+        this.rentActiveButton = this.shadow.querySelector('#rant-alert-activate-button');
+        this.subscribeActiveButton = this.shadow.querySelector('#subscribe-activate-button');
     };
 
     listeners() {
+        this.followerActiveButton.addEventListener('click', () => {
+            this.followerButtonState = !this.followerButtonState;
+            if (this.followerButtonState) {
+                this.followerActiveButton.style.backgroundColor = "greenyellow"
+            } else {
+                this.followerActiveButton.style.backgroundColor = "gray";
+            };
+        });
 
+        this.rentActiveButton.addEventListener('click', () => {
+            this.rentButtonState = !this.rentButtonState;
+            if (this.rentButtonState) {
+                this.rentActiveButton.style.backgroundColor = "greenyellow"
+            } else {
+                this.rentActiveButton.style.backgroundColor = "gray";
+            };
+        })
+
+        this.subscribeActiveButton.addEventListener('click', () => {
+            this.subscribeButtonState = !this.subscribeButtonState;
+            if (this.subscribeButtonState) {
+                this.subscribeActiveButton.style.backgroundColor = "greenyellow";
+            } else {
+                this.subscribeActiveButton.style.backgroundColor = "gray";
+            };
+        });
     };
 
     customEvents() {
