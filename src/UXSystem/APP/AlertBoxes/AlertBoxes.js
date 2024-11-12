d957b913bb92586fa425fca34167e85373296717
xXMaddyX <maddyaion@hotmail.de>
2024-11-12 11:37:02 +0100

    Aupdate UI Behavior

--- src/UXSystem/APP/AlertBoxes/AlertBoxes.js
+++ src/UXSystem/APP/AlertBoxes/AlertBoxes.js
@@ -4,9 +4,10 @@ class AlertBoxes extends HTMLElement {
     constructor() {
         super();
         this.shadow = this.attachShadow({mode: "open"});
-        this.followerButtonState = false;
-        this.rentButtonState = false;
-        this.subscribeButtonState = false;
+        this.activeButton = null;
+        this.alertObjList = [];
+        this.rentObjList = [];
+        this.subObjList = [];
     };
 
     async connectedCallback () {
@@ -32,50 +33,41 @@ class AlertBoxes extends HTMLElement {
         this.subBoxBody = this.shadow.querySelector('.sub-boxes-body');
     };
 
-    setButtonCondition(state, button) {
-        if (state === true) {
-            button.style.backgroundColor = "greenyellow";
-        } else {
-            button.style.backgroundColor = "gray";
-        };
+
+    setActiveButton(button) {
+        if (this.activeButton) {
+            this.activeButton.style.backgroundColor = "gray";
+        }
+        this.activeButton = button;
+        this.activeButton.style.backgroundColor = "greenyellow";
     };
 
     listeners() {
-        this.followerActiveButton.addEventListener('click', () => {
-            this.followerButtonState = !this.followerButtonState;
-            this.setButtonCondition(this.followerButtonState, this.followerActiveButton)
-        });
-
-        this.rentActiveButton.addEventListener('click', () => {
-            this.rentButtonState = !this.rentButtonState;
-            this.setButtonCondition(this.rentButtonState, this.rentActiveButton);
-        })
-
-        this.subscribeActiveButton.addEventListener('click', () => {
-            this.subscribeButtonState = !this.subscribeButtonState;
-            this.setButtonCondition(this.subscribeButtonState, this.subscribeActiveButton);
-        });
+        this.followerActiveButton.addEventListener('click', () => this.setActiveButton(this.followerActiveButton));
+        this.rentActiveButton.addEventListener('click', () => this.setActiveButton(this.rentActiveButton));
+        this.subscribeActiveButton.addEventListener('click', () => this.setActiveButton(this.subscribeActiveButton));
 
         this.addEventListener("send-alert-data-to-alert-box", (e) => {
             let data = e.detail.data;
-            if (this.followerButtonState) {
-                const newButton = document.createElement("button");
-                newButton.innerText = e.detail.data.sourceName;
+            const newButton = document.createElement("button");
+            newButton.innerText = data.sourceName;
+
+            if (this.activeButton === this.followerActiveButton) {
                 this.alertBoxBody.append(newButton);
-            } else if (this.rentButtonState) {
-                const newButton = document.createElement("button");
-                newButton.innerText = e.detail.data.sourceName;
+                this.alertObjList.push(data);
+
+            } else if (this.activeButton === this.rentActiveButton) {
                 this.rentBoxBody.append(newButton);
-            } else if (this.subscribeButtonState) {
-                const newButton = document.createElement("button");
-                newButton.innerText = e.detail.data.sourceName;
+                this.rentObjList.push(data);
+
+            } else if (this.activeButton === this.subscribeActiveButton) {
                 this.subBoxBody.append(newButton);
+                this.subObjList.push(data);
             } else {
-                alert("no Box selected")
-            }
-            
-            console.log(data)
-        })
+                alert("No Alert Box selected");
+            };
+            console.log(data);
+        });
     };
 
     handelIncomeData(data) {
